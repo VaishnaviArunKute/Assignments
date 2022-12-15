3e57ab0108b28fed2f7ebd657539853a613898f9
VaishnaviArunKute <113573706+VaishnaviArunKute@users.noreply.github.com>
2022-12-15 13:28:51 +0530

    Update ass8_4.py

--- ass8/ass8_4.py
+++ ass8/ass8_4.py
@@ -1,15 +1,30 @@
 import matplotlib.pyplot as plt
-#x = [9,6,7,0,8,18]
-#y = [6,12,7,5,12,5]
+'''
 x = [1,-2,-1,-8,0,10]
 y = [-2,4,-1,-3,4,-3]
 plt.plot(x,y)
 plt.xlabel('x - axis')
-
 plt.ylabel('y - axis')
-  
 plt.title('My first graph!')
-  
 plt.show()
+'''
 
-[(1,-2), (-2, 4), (-1,-1),(-8, -3), (0, 4), (10,-3)]
+Input =  [(1,-2), (-2, 4), (-1,-1),(-8, -3), (0, 4), (10,-3)]
+Output=[]
+for i in Input:
+    new=[]
+    for j in i:
+        j=j+8
+        new.append(j)
+    Output.append(tuple(new))
+print("the set of positive co-ordinates are: ",Output)
+
+'''
+x = [9,6,7,0,8,18]
+y = [6,12,7,5,12,5]
+plt.plot(x,y)
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.title('My first graph!')
+plt.show()
+'''
